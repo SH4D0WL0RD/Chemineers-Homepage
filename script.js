ed7248eb9bb8f339e482d7f10bec9720be4a57ae
SH4D0WL0RD <157052375+SH4D0WL0RD@users.noreply.github.com>
2024-03-20 17:34:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,3 @@
-
-// let items = document.querySelectorAll('.slider .list .item');
-// let next = document.getElementById('next');
-// let prev = document.getElementById('prev');
-// let dots = document.querySelectorAll('.slider .dots li');
-
-// let lengthItems = items.length - 1;
-// let active = 0;
-// next.onclick = function(){
-//     active = active + 1 <= lengthItems ? active + 1 : 0;
-//     reloadSlider();
-// }
-// prev.onclick = function(){
-//     active = active - 1 >= 0 ? active - 1 : lengthItems;
-//     reloadSlider();
-// }
-// let refreshInterval = setInterval(()=> {next.click()}, 3000);
-// function reloadSlider(){
-//     slider.style.left = -items[active].offsetLeft + 'px';
-//     // 
-//     let last_active_dot = document.querySelector('.slider .dots li.active');
-//     last_active_dot.classList.remove('active');
-//     dots[active].classList.add('active');
-
-//     clearInterval(refreshInterval);
-//     refreshInterval = setInterval(()=> {next.click()}, 3000);
-
-    
-// }
-
-// dots.forEach((li, key) => {
-//     li.addEventListener('click', ()=>{
-//          active = key;
-//          reloadSlider();
-//     })
-// })
-// window.onresize = function(event) {
-//     reloadSlider();
-// };
 const slides = document.querySelectorAll('.slide');
 const next = document.getElementById('next');
 const prev = document.getElementById('prev');
@@ -83,9 +44,6 @@ prev.addEventListener('click', e => {
     }
 });
 
-
-
-//auto slide
 if(auto) {
     slideInterval = setInterval(nextSlide, intervalTime);
 }
